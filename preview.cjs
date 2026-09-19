e2752bbe5f49d063895e9d07f3f44893c3c06e7a
qwer13270 <kcbs96199@gmail.com>
2026-09-19 00:28:26 -0500

    Build editorial marketing portfolio for Yunyi Qian

---
+++ preview.cjs
@@ -0,0 +1,12 @@
+const http = require('node:http');
+const fs = require('node:fs');
+const path = require('node:path');
+const root = path.join(__dirname, 'dist');
+http.createServer((req, res) => {
+  const pathname = new URL(req.url, 'http://localhost').pathname;
+  const files = { '/': ['index.html', 'text/html'], '/styles.css': ['styles.css', 'text/css'] };
+  const asset = files[pathname];
+  if (!asset) { res.writeHead(404); res.end('Not found'); return; }
+  res.writeHead(200, { 'Content-Type': `${asset[1]}; charset=utf-8` });
+  fs.createReadStream(path.join(root, asset[0])).pipe(res);
+}).listen(4173, '127.0.0.1', () => console.log('Local: http://127.0.0.1:4173'));
